5dc09264b31b1deab198a0582e4b0e851552b628
VictorAlexandre090 <144179074+VictorAlexandre090@users.noreply.github.com>
2023-10-27 22:41:18 +0000

    Fri, Oct 27, 2023, 7:41 PM -03:00

--- projeto008/consultaIntegracao.py
+++ projeto008/consultaIntegracao.py
@@ -1,10 +1,11 @@
-# import required modules
-import requests, json, sys, pprint
-api_key = ""
+import requests, json, pprint
+api_key = "61178d1f"
 nome_do_filme = "Titanic"
 base_url = "http://www.omdbapi.com/?"
-#complete_url = base_url + "apikey=" + api_key + "&i=tt3896198"
-complete_url = base_url + "apikey=" + api_key + "&t="+ nome_do_filme
+#complete_url = base_url + "apikey=" + api_key + "?i=tt3896198"
+complete_url = base_url + "apikey=" + api_key + "&t=" + nome_do_filme
 response = requests.get(complete_url)
+
 x = response.json()
 pprint.pprint(x)
+#i=tt3896198&apikey=61178d1f"
