d6bd160f55f8a5d9082f70e3d50e0fd3353a546b
VictorAlexandre090 <144179074+VictorAlexandre090@users.noreply.github.com>
2023-10-30 20:59:09 +0000

    Mon, Oct 30, 2023, 5:59 PM -03:00

---
+++ projeto012/main.py
@@ -0,0 +1,28 @@
+import fastapi
+import connection
+app = fastapi.FastAPI()
+@app.get("/")
+def hello_world_root():
+    return {"Olá": "Funciona"}
+
+@app.get("/getContinents")
+def getContinents():
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql = "select * from continents"
+    mycursor.execute(sql)
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continents": Continents_list}
+
+@app.get("/getContinent/{continent_id}")
+def getContinent(continent_id : int):
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql="select * from continents where continent_id = {0}".format(continent_id)
+    mycursor.execute(sql)
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continent": Continents_list}
