956a55460190df850d93f978f363ce4cca153d6c
VictorAlexandre090 <144179074+VictorAlexandre090@users.noreply.github.com>
2023-10-31 21:58:31 +0000

    Tue, Oct 31, 2023, 6:58 PM -03:00

--- projeto012/app.py
+++ projeto012/app.py
@@ -1,5 +1,6 @@
 import flask
 import requests
+import json2table
 app = flask.Flask(__name__)
 
 @app.route("/")
@@ -8,10 +9,28 @@ def OlaMundo():
 
 @app.route("/consultaContinentes")
 def consultaContinentes():
-    response2 = requests.get("http://127.0.0.1:8000/getContinents")
-    return response2.json()
+        response2 = requests.get("http://127.0.0.1:8000/getContinents")
+
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
+
+@app.route("/consultaRegioes")
+def consultaRegioes():
+        response2 = requests.get("http://127.0.0.1:8000/getRegions")
+
+        json_object = response2.json()
+        build_direction = "LEFT_TO_RIGHT"
+        table_attributes = {"style" : "width:100%", "border": "1px solid black"}
+        html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+        #print(html)
+        #return response2.json()
+        return html
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
