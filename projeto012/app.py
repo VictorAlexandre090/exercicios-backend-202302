5dd62552d23672725f77adf038eae5dfc12ff171
VictorAlexandre090 <144179074+VictorAlexandre090@users.noreply.github.com>
2023-10-31 21:50:31 +0000

    Tue, Oct 31, 2023, 6:50 PM -03:00

--- projeto012/app.py
+++ projeto012/app.py
@@ -1,8 +1,18 @@
 import flask
+import requests
 app = flask.Flask(__name__)
 
 @app.route("/")
-def OlaMundo ():
+def OlaMundo():
         return "<p>Ola Mundo</p>"
+
+@app.route("/consultaContinentes")
+def consultaContinentes():
+    response2 = requests.get("http://127.0.0.1:8000/getContinents")
+    return response2.json()
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
