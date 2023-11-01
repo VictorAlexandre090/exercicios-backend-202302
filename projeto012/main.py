45bbd9025aeefb6ca40e39378e6978cd4e36bc5c
VictorAlexandre090 <144179074+VictorAlexandre090@users.noreply.github.com>
2023-11-01 20:43:46 +0000

    Wed, Nov 1, 2023, 5:43 PM -03:00

--- projeto012/main.py
+++ projeto012/main.py
@@ -1,5 +1,9 @@
 import fastapi
 import connection
+import json
+from pydantic import BaseModel, Field
+
+
 app = fastapi.FastAPI()
 @app.get("/")
 def hello_world_root():
@@ -16,6 +20,46 @@ def getContinents():
     mycursor.close()
     return {"Continents": Continents_list}
 
+@app.get("/getContinent/{continent_id}")
+def getContinent(continent_id : int):
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql="select * from continents where continent_id = {0}".format(continent_id)
+    mycursor.execute(sql)
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continent": Continents_list}
+
+#Refazer!!!
+@app.post('/createContinent/{continent_name}')
+def createContinent(continent_name: str):
+    if continent_name == "":
+        return {'Error': 'Item vazio'}
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql=" INSERT INTO continents (name) values ('{0}')".format(continent_name)
+    mycursor.execute(sql)
+    mycursor.close()
+    return {"Continent: OK"}
+
+class Continent(BaseModel):
+   continent_name :str = Field(None, title="nome dos continentes", max_length=25)
+
+@app.post('/createContinent2')
+def createContinent2(info: Continent):
+    #Continent_details = json.loads(Continent)
+    if info.continent_name == "":
+        return {'Error': 'Item vazio'}
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql=" INSERT INTO continents (name) values ('{0}')".format(info.continent_name)
+    mycursor.execute(sql)
+    mycursor.close()
+    return {
+        "status" : "SUCCESS",
+        "data" : info.continent_name
+    }
+
+
 @app.get("/getRegions")
 def getRegions():
     Regions_list = []
@@ -27,14 +71,14 @@ def getRegions():
     mycursor.close()
     return {"Regions": Regions_list}
 
-@app.get("/getLanguages")
-def getLanguages():
-    Languages_list = []
-    mycursor = connection.mydb.cursor(dictionary=True)
-    sql = "select * from languages"
-    mycursor.execute(sql)
-    for data_languages in mycursor:
-        Languages_list.append( data_languages )
-    mycursor.close()
-    return {"Languages": Languages_list}
 
+@app.get("/getRegion/{continent_id}")
+def getRegion(continent_id : int):
+    Regions_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql="select * from regions where continent_id = {0}".format(continent_id)
+    mycursor.execute(sql)
+    for data_regions in mycursor:
+        Regions_list.append( data_regions )
+    mycursor.close()
+    return {"Region": Regions_list}
